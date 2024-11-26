c495104c595c2d7c8802f21bb59c469b1cd736f0
VVolfBite <1327256274@qq.com>
2024-11-26 16:46:08 +0800

    1

--- dash_page/app.py
+++ dash_page/app.py
@@ -1,61 +1,117 @@
-import pickle
-import sys
-from dash import Dash, html, dcc,Input,Output
-
+from dash import Dash, html, Input, Output, State, dcc
 import dash_bootstrap_components as dbc
-from streamlit import button
-WORK_DIRECTORY = "D:/WarnoAntlr-main/"
-sys.path.append(WORK_DIRECTORY)
-from dash_page.tool.component import *
 
+# 自定义 HTML 模板，为 body 标签添加 id
 app = Dash(
     __name__,
     external_scripts=[
         "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     ],
     external_stylesheets=[
-        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
         "https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css",
     ],
+    index_string="""
+                <!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                        {%metas%}
+                        <title>{%title%}</title>
+                        {%favicon%}
+                        {%css%}
+                    </head>
+                    <body id="body" data-bs-theme="light">
+                        {%app_entry%}
+                        <footer>
+                            {%config%}
+                            {%scripts%}
+                            {%renderer%}
+                        </footer>
+                    </body>
+                </html>
+                """,
 )
+# 将主题挂载在body下,防止临时生成的页面没有挂载在layout下
+app.clientside_callback(
+    """
+    function(theme) {
+        const body = document.getElementById("body");
+        if (body) {
+            body.setAttribute("data-bs-theme", theme);
+        }
+        return null; // 返回值不影响
+    }
+    """,
+    Input("theme-store", "data"),
+)
+# 布局设置
 
-side_content = html.Div(
-    children=[
-        Dropdown("query_type_dropdown","选择查询的类型",["弹药","武器","单位","特性","机制"]),
-        FloatingDropdown(input_id="dropdown1", label="Search and Select", suggestions = ["Option 1", "Option 2", "Option 3"], explanation = "Type to filter options.")
-
+offcanvas = html.Div(
+    [
+        dcc.Store(id="theme-store", data="light"),
+        dbc.Button(
+            html.I(className="bi bi-code"),
+            id="open-offcanvas",
+            style={
+                "position": "fixed",
+                "left": "10px",
+                "bottom": "10px",
+                "zIndex": "1050",
+            },
+            n_clicks=0,
+        ),
+        dbc.Button(
+            html.I(className="bi bi-brightness-high"),
+            id="change-theme",
+            style={
+                "position": "fixed",
+                "left": "10px",
+                "bottom": "50px",
+                "zIndex": "1050",
+            },
+            n_clicks=0,
+        ),
+        dbc.Offcanvas(
+            html.P(
+                "This is the content of the Offcanvas. "
+                "Close it by clicking on the close button, or "
+                "the backdrop."
+            ),
+            id="offcanvas",
+            title=html.H1("Title"),
+        ),
     ]
 )
 
-side_bar = OffCanvasSidebar(
-    offcanvas_id="sidebar",
-    button_class="btn btn-primary",
-    side_content=side_content,
-    side_header="Sidebar",
-    position="start",
-)
+
+app.layout = html.Div(offcanvas)
 
 
-
-
-app_layout = Page(page_header=html.Div(), sections=side_bar)  # 空的页面头部
-
-app.layout = html.Div(app_layout, id= "top_page",**{"data-bs-theme": "dark"})
-
-# CallBack 部分
-## 主题切换
+# 回调函数：切换主题状态
 @app.callback(
-    [Output("top_page", "data-bs-theme"),
-     Output("button_theme", "children")],
-    Input("button_theme", "n_clicks"),
-    prevent_initial_call=True,
+    Output("theme-store", "data"),
+    Output("change-theme", "children"),
+    Input("change-theme", "n_clicks"),
+    State("theme-store", "data"),
 )
-def toggle_theme(n_clicks):
-    if n_clicks % 2 == 0:
-        return "light",[html.I(className="bi bi-brightness-high")]
-    else:
-        return "dark" ,html.I(className="bi bi-moon")
+def toggle_theme(n1, current_theme):
+    if n1 and current_theme == "dark":
+        return "light", html.I(className="bi bi-brightness-high")
+    if n1 and current_theme == "light":
+        return "dark", html.I(className="bi bi-moon")
+    return current_theme, html.I(
+        className="bi bi-brightness-high" if current_theme == "light" else "bi bi-moon"
+    )
 
+# 回调函数：开关侧边栏
+@app.callback(
+    Output("offcanvas", "is_open"),
+    Input("open-offcanvas", "n_clicks"),
+    [State("offcanvas", "is_open")],
+)
+def toggle_offcanvas(n1, is_open):
+    if n1:
+        return not is_open
+    return is_open
 
 if __name__ == "__main__":
     app.run_server(debug=True)
--- dash_page/tool/component.py
+++ dash_page/tool/component.py
@@ -459,7 +459,6 @@ def Accordion(
         children=accordion_items,
     )
 
-
 def Dropdown(drop_down_id:str, button_name: str, options: list[str]):
     return html.Div(
         className="dropdown mt-3",
